f18cb8a9fdf34bca21612be90c321e1a5884ddb0
Trisha Gee <trisha.gee@gmail.com>
2021-05-27 12:11:00 +0200

    Example code for Alt+Enter.

---
+++ src/error/java/org/effectiveintellij/error/AltEnterError.java
@@ -0,0 +1,15 @@
+package org.effectiveintellij.error;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static java.util.Arrays.sort;
+
+public class AltEnterError {
+    private final String[] stringArray = new String[]{"IntelliJ IDEA", "Space", "Code With Me", "TeamCity"};
+
+    public void error() {
+        List<String> strings = new HashMap<Integer, String>();
+    }
+
+}
