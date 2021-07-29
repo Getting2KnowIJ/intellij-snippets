df8e9c558feb3a621f29defb3b3bd019daa3fe50
Trisha Gee <trisha.gee@gmail.com>
2021-07-29 16:54:33 +0200

    Corrected formatting in ExampleImplementation

--- src/main/java/org/effectiveintellij/navigation/ExampleImplementation.java
+++ src/main/java/org/effectiveintellij/navigation/ExampleImplementation.java
@@ -1,6 +1,6 @@
 package org.effectiveintellij.navigation;
 
-public class ExampleImplementation implements ExampleInterface{
+public class ExampleImplementation implements ExampleInterface {
     @Override
     public void exampleMethod() {
         // implements a method from the class's interface
