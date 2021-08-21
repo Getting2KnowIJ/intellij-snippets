a6c93eb5e6e830e9c3936133e194622dba26a568
Trisha Gee <trisha.gee@gmail.com>
2021-08-21 06:57:07 +0100

    Example for Selection

---
+++ src/main/java/org/effectiveintellij/editor/Customer.java
@@ -0,0 +1,4 @@
+package org.effectiveintellij.editor;
+
+record Customer(long id, String name) {
+}
---
+++ src/main/java/org/effectiveintellij/editor/Selection.java
@@ -0,0 +1,15 @@
+package org.effectiveintellij.editor;
+
+public class Selection {
+    public static void printCustomerDetails() {
+        Customer customer = new Customer(6365, "customer");
+        System.out.println(printableDetails(customer));
+    }
+
+    private static String printableDetails(Customer customer) {
+        return "{" +
+                "id=" + customer.id() +
+                ", name='" + customer.name() + '\'' +
+                "}";
+    }
+}
