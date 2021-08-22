293c8782c2d9f4f4e3375ef8086015a12099cb9a
Trisha Gee <trisha.gee@gmail.com>
2021-08-22 11:42:29 +0100

    Formatting the long string again, this time to try to get something that wraps sensibly in the text of the book

--- src/main/java/org/effectiveintellij/editor/LongStrings.java
+++ src/main/java/org/effectiveintellij/editor/LongStrings.java
@@ -1,12 +1,13 @@
 package org.effectiveintellij.editor;
 
 public class LongStrings {
-    private final String aLongString = "Try out some of the editor tips on this long " +
-                                       "string, for example, pressing Enter in the " +
-                                       "middle of a line, using (**⌃⇧J** | **Ctrl+Shift+J**) " +
-                                       "on a line to join it with the line below, or " +
-                                       "using Alt+Enter to inject a language, like " +
-                                       "markdown.";
+    private final String aLongString = "Try out some of the editor tips on " +
+                                       "this long string, for example, " +
+                                       "pressing Enter in the middle of a line, " +
+                                       "using (**⌃⇧J** | **Ctrl+Shift+J**) on " +
+                                       "a line to join it with the line below, " +
+                                       "or using Alt+Enter to inject a " +
+                                       "language, like markdown.";
 
     private final String json = "{\n" +
                                 "    \"name\":\"theName\",\n" +
