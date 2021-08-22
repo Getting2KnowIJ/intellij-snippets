5b8ada316f317a809a8754622f76fa9693095be9
Trisha Gee <trisha.gee@gmail.com>
2021-08-22 08:13:53 +0100

    Formatting the long string.

--- src/main/java/org/effectiveintellij/editor/LongStrings.java
+++ src/main/java/org/effectiveintellij/editor/LongStrings.java
@@ -1,14 +1,20 @@
 package org.effectiveintellij.editor;
 
 public class LongStrings {
-    private final String aLongString = "Try out some of the editor tips on this long string, for example, pressing " +
-            "Enter in the middle of a line, using (**⌃⇧J** | **Ctrl+Shift+J**) on a line to join it with the " +
-            "line below, or using Alt+Enter to inject a language, like markdown.";
+    private final String aLongString = "Try out some of the editor tips on this long " +
+                                       "string, for example, pressing Enter in the " +
+                                       "middle of a line, using (**⌃⇧J** | **Ctrl+Shift+J**) " +
+                                       "on a line to join it with the line below, or " +
+                                       "using Alt+Enter to inject a language, like " +
+                                       "markdown.";
+
     private final String json = "{\n" +
                                 "    \"name\":\"theName\",\n" +
                                 "    \"id\":12432\n" +
                                 "}\n";
+
     private final String sql = "SELECT * " +
                                "FROM Customer c" +
                                "WHERE c.id = ?";
+
 }
