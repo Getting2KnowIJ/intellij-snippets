c8e0d61881290c361361ddbd63ad4dd8f94c87df
Trisha Gee <trisha.gee@gmail.com>
2021-10-10 07:18:34 +0200

    Fully qualified name for the error example.

--- src/error/java/com/getting2knowij/error/AltEnterError.java
+++ src/error/java/com/getting2knowij/error/AltEnterError.java
@@ -1,15 +1,11 @@
 package com.getting2knowij.error;
 
 import java.util.HashMap;
-import java.util.List;
-
-import static java.util.Arrays.sort;
 
 public class AltEnterError {
-    private final String[] stringArray = new String[]{"IntelliJ IDEA", "Space", "Code With Me", "TeamCity"};
 
     public void error() {
-        List<String> strings = new HashMap<Integer, String>();
+        java.util.List<String> strings = new HashMap<Integer, String>();
     }
 
 }
