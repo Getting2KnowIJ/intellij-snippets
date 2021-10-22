b66bc51d9fd1a88c4f95a602047872c065946ca9
Trisha Gee <trisha.gee@gmail.com>
2021-10-22 11:51:00 +0200

    Improving GreetMe

--- src/main/java/com/getting2knowij/running/GreetMe.java
+++ src/main/java/com/getting2knowij/running/GreetMe.java
@@ -4,7 +4,8 @@ public class GreetMe {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("You need to tell me your name!");
+            System.exit(0);
         }
-        System.out.printf("Hello %s", args[0]);
+        System.out.printf("Hello %s!\n", args[0]);
     }
 }
