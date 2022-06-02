02d38758f4cb098488bcc3e3fc30fdb0827b41ec
Trisha Gee <trisha.gee@gmail.com>
2022-06-02 11:21:54 +0200

    Examples for Diving into Debugging

---
+++ src/main/java/com/getting2knowij/debugging/SquareApplication.java
@@ -0,0 +1,11 @@
+package com.getting2knowij.debugging;
+
+public class SquareApplication {
+    public static void main(String[] args) {
+        Square square = new Square("Square for running");
+
+        String name = square.getName();
+
+        System.out.println(name);
+    }
+}
