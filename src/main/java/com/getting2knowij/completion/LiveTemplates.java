5bcde8bd60b06a69fbc5a51b069b160696731422
Trisha Gee <trisha.gee@gmail.com>
2022-08-06 17:07:22 +0100

    LiveTemplates examples

---
+++ src/main/java/com/getting2knowij/completion/LiveTemplates.java
@@ -0,0 +1,14 @@
+package com.getting2knowij.completion;
+
+public class LiveTemplates {
+  // type prsf and press tab
+  // type "main" and press tab
+
+  void systemOutExample() {
+    // type "sout" and press tab
+  }
+
+  void forExample() {
+    // type "fori" and press tab
+  }
+}
