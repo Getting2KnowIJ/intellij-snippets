21eaea92625ed01432690ab9093fa657171e7750
Trisha Gee <trisha.gee@gmail.com>
2022-09-08 15:30:29 +0200

    Example for TODO window.

---
+++ src/main/java/com/getting2knowij/editor/TodoNotes.java
@@ -0,0 +1,9 @@
+package com.getting2knowij.editor;
+
+public class TodoNotes {
+  public static void main(String[] args) {
+    // TODO: replace hard-coded string with i18n token
+    var message = "Hello and welcome";
+    System.out.println(message);
+  }
+}
