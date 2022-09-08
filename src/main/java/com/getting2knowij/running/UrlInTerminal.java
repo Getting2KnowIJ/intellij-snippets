ebc59f607fe42d976be8b236f7e8466a06ae0faa
Trisha Gee <trisha.gee@gmail.com>
2022-09-08 15:30:09 +0200

    Example showing how URLs are rendered in the terminal (so you can click on them).

---
+++ src/main/java/com/getting2knowij/running/UrlInTerminal.java
@@ -0,0 +1,8 @@
+package com.getting2knowij.running;
+
+public class UrlInTerminal {
+  public static void main(String[] args) {
+    String url = "https://github.com/Getting2KnowIJ";
+    System.out.printf("See %s for more info", url);
+  }
+}
