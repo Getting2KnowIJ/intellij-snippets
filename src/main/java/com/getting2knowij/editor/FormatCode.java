e223cb6d749bbd67e6bc84f9568f2ffeb8380f90
Trisha Gee <trisha.gee@gmail.com>
2022-09-30 12:33:42 +0200

    An example for code formatting

---
+++ src/main/java/com/getting2knowij/editor/FormatCode.java
@@ -0,0 +1,13 @@
+package com.getting2knowij.editor;
+
+public class FormatCode {
+  void tryFormattingThisMethod() {
+    // use (⌘⌥L | Ctrl+Alt+L) to format this whole class file
+      int val = 0;
+    for (int i = 0; i <5; i++) {
+      if (val == 4) System.out.println(val);
+        val = i;
+      }
+    // then try selecting just part of the method and pressing (⌘⌥L | Ctrl+Alt+L)
+  }
+}
