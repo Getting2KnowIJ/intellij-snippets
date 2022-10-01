1b3aa0ddcc4d16265b01731fac6f980819d91054
Trisha Gee <trisha.gee@gmail.com>
2022-10-01 18:54:52 +0200

    Adding an example for changing the method signature

---
+++ src/main/java/com/getting2knowij/refactoring/Circle.java
@@ -0,0 +1,19 @@
+package com.getting2knowij.refactoring;
+
+import java.awt.*;
+
+public class Circle {
+  private String name;
+
+  public Circle(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void paintTo(Canvas canvas) {
+    // do the paint
+  }
+}
