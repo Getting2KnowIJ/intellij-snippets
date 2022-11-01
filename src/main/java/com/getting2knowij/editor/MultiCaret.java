7674e2c8f2dee5b6cb2a58697e62ae5f0fe5c5bd
Trisha Gee <trisha.gee@gmail.com>
2022-11-01 16:46:46 +0100

    Adding code for Multi caret.

--- src/main/java/com/getting2knowij/editor/MultiCaret.java
+++ src/main/java/com/getting2knowij/editor/MultiCaret.java
@@ -1,6 +1,6 @@
 package com.getting2knowij.editor;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "FieldMayBeFinal"})
 public class MultiCaret {
   private long id;
   private String firstName;
@@ -8,8 +8,16 @@ public class MultiCaret {
   private int age;
   private String additionalNotes;
 
-  private final String storageFilename = "customer";
-  private final String logFilename = "log";
+  private final String storageFilename = "customer.txt";
+  private final String logFilename = "log.txt";
+
+  public MultiCaret(long id, String firstName, String lastName, int age, String additionalNotes) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.additionalNotes = additionalNotes;
+  }
 
   @Override
   public String toString() {
