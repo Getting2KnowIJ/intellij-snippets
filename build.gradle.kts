b2bd159862805fd30b17cff99ce5f17c497e0125
Trisha Gee <trisha.gee@gmail.com>
2023-11-29 10:20:26 +0100

    Changed to Kotlin build file.

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    java
+}
+
+group = "org.getting2knowij"
+version = "1.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
