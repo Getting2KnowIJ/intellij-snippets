1e16ee6dd1da8dc5ed2b58c0a37af9a81cb15363
Trisha Gee <trisha.gee@gmail.com>
2023-12-04 20:05:15 +0100

    Adding another method to the formatting examples so you can see selective formatting

--- src/main/java/com/getting2knowij/editor/FormatCode.java
+++ src/main/java/com/getting2knowij/editor/FormatCode.java
@@ -7,14 +7,18 @@ import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public class FormatCode {
+  void anotherBadlyFormattedMethod(int arg) {
+    if (arg == 1)
+        arg++;
+      System.out.println(arg);
+  }
+
     void tryFormattingThisMethod() {
-    // use (⌘⌥L | Ctrl+Alt+L) to format this whole class file
       int val = 0;
     for (int i = 0; i <5; i++) {
       if (val == 4) System.out.println(val);
         val = i;
       }
-    // then try selecting just part of the method and pressing (⌘⌥L | Ctrl+Alt+L)
   }
 
   void methodToChangeFormat() throws IOException {
