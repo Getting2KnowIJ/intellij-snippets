cf11b38ec756d713a51f7d346cc743da28ae38de
Trisha Gee <trisha.gee@gmail.com>
2023-12-08 09:19:47 +0100

    Drafting the idea

--- src/main/java/com/getting2knowij/vcs/Hello.java
+++ src/main/java/com/getting2knowij/vcs/Hello.java
@@ -1,7 +1,11 @@
 package com.getting2knowij.vcs;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello Git!");
+  }
+
+  private void oldMethod() {
+      // do something
+  }
 }
