aacd2c97cacf0758c8a7f4173f823d6caed15d77
Trisha Gee <trisha.gee@gmail.com>
2023-12-08 09:11:07 +0100

    Making a start on the Greet task

---
+++ src/main/java/com/getting2knowij/vcs/Hello.java
@@ -0,0 +1,7 @@
+package com.getting2knowij.vcs;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/getting2knowij/vcs/OldFile.java
@@ -0,0 +1,5 @@
+package com.getting2knowij.vcs;
+
+public class OldFile {
+    // TODO: delete this
+}
