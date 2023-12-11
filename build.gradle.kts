2f3e069d141b69d749805fdb856af640b46e0318
Trisha Gee <trisha.gee@gmail.com>
2023-12-11 15:04:05 +0100

    Adding a version to the junit platform to prevent warnings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
 }
 
 tasks.test {
