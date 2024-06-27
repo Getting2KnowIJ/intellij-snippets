47c0088dfd150e9b3f4c2cca41771c7a9949028b
Trisha <trisha.gee@gmail.com>
2024-06-27 12:45:39 +0200

    Upgrade Gradle, Java and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
 }
 
 group = "org.getting2knowij"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
 tasks.test {
