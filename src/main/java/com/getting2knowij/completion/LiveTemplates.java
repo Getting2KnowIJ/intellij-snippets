df79ee8b55a34ab4bec186d1d417a7595ee28a62
Trisha <trisha.gee@gmail.com>
2024-08-22 13:34:44 +0200

    Adding an example for "iter" Live Template

--- src/main/java/com/getting2knowij/completion/LiveTemplates.java
+++ src/main/java/com/getting2knowij/completion/LiveTemplates.java
@@ -1,5 +1,8 @@
 package com.getting2knowij.completion;
 
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class LiveTemplates {
   // type prsf and press tab
   // type "main" and press tab
@@ -10,5 +13,11 @@ public class LiveTemplates {
 
   void forExample() {
     // type "fori" and press tab
+  }
+
+  void iterExample() {
+    List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    // type "iter" and press tab
   }
 }
