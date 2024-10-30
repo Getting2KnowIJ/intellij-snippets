e0e379274b56c7447f32a1b4c17da02049d24cbd
Trisha <trisha.gee@gmail.com>
2024-10-30 15:27:10 +0100

    Upgrade Junit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.3")
 }
 
 tasks.test {
