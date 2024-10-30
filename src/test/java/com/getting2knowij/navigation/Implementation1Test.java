fe4df711d795b756a832b1faf071f96ba294d3da
Trisha <trisha.gee@gmail.com>
2024-10-30 15:27:31 +0100

    Adding skeleton tests for the interfaces

---
+++ src/test/java/com/getting2knowij/navigation/Implementation1Test.java
@@ -0,0 +1,13 @@
+package com.getting2knowij.navigation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Implementation1Test {
+    @Test
+    public void testAMethodHere() {
+        Implementation1 implementation = new Implementation1();
+        assertDoesNotThrow(implementation::aMethodHere);
+    }
+}
---
+++ src/test/java/com/getting2knowij/navigation/Implementation2Test.java
@@ -0,0 +1,13 @@
+package com.getting2knowij.navigation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Implementation2Test {
+    @Test
+    void testAMethodHere() {
+        Implementation2 implementation = new Implementation2();
+        assertDoesNotThrow(implementation::aMethodHere);
+    }
+}
