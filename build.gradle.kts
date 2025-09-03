c77284aacad3804367f804e47d5b1b4835b6b1c9
Trisha Gee <trisha.gee@gmail.com>
2025-09-03 17:14:09 +0200

    Upgrade to Gradle 9; Upgrade to Java 24; Upgrade Junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,21 @@ plugins {
 }
 
 group = "org.getting2knowij"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
 }
 
 tasks.test {
